e6a53f56f25d61d7331abd968c40e05682637242
luiscota <luismigcota@gmail.com>
2019-11-25 12:04:53 -0700

    modificacion visual para cambiar el mouse por una mano

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -2,12 +2,7 @@
   <v-flex xs3 pa-1>
     <v-hover>
       <v-card class="mx-auto" color="gray lighten-4">
-        <v-img
-          v-if="image"
-          src="https://picsum.photos/id/11/500/300"
-          :aspect-ratio="16 / 9"
-        >
-        </v-img>
+        <v-img v-if="image" src="https://picsum.photos/id/11/500/300" :aspect-ratio="16 / 9"></v-img>
 
         <v-card-text class="pt-4" style="position: relative;">
           <v-btn
@@ -23,18 +18,21 @@
           >
             <v-icon>mdi-plus</v-icon>
           </v-btn>
-          <div class="subtitle-2 mb-2">
-            {{ producto.nombre }}
-          </div>
+          <div class="subtitle-2 mb-2">{{ producto.nombre }}</div>
           <div class="caption">${{ producto.amount }}</div>
           <v-row no-gutters>
-            <v-col sm="8" class="caption"
-              >Cantidad:<span class="ml-2">{{ quantity }}</span></v-col
-            >
+            <v-col sm="8" class="caption">
+              Cantidad:
+              <span class="ml-2">{{ quantity }}</span>
+            </v-col>
 
-            <v-col sm="4" class=" text-right">
-              <span @click="restartCantidad"><v-icon>mdi-minus</v-icon></span
-              ><span @click="agregarCantidad"><v-icon>mdi-plus</v-icon></span>
+            <v-col sm="4" class="text-right">
+              <span class="mouseOver" @click="restartCantidad">
+                <v-icon>mdi-minus</v-icon>
+              </span>
+              <span class="mouseOver" @click="agregarCantidad">
+                <v-icon>mdi-plus</v-icon>
+              </span>
             </v-col>
           </v-row>
         </v-card-text>
@@ -76,3 +74,9 @@ export default {
   }
 };
 </script>
+
+<style>
+.mouseOver {
+  cursor: pointer;
+}
+</style>
